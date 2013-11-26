893dfdf0d848ebcda78f5887b8dcb8cbb4dfc0be
arsene tchangou <arsene tchangou@130.83.119.194>
2013-11-26 19:42:33 +0100

    vorbereitung 4.Pka

--- src/fernseher/Hauptfenster.java
+++ src/fernseher/Hauptfenster.java
@@ -1,6 +1,7 @@
 package fernseher;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.EventQueue;
 
 import javax.swing.JComponent;
@@ -19,6 +20,7 @@ import java.awt.event.MouseMotionAdapter;
 import java.awt.event.MouseEvent;
 import java.util.HashSet;
 import java.util.Set;
+import java.awt.event.MouseAdapter;
 
 public class Hauptfenster extends JFrame {
 
@@ -48,8 +50,19 @@ public class Hauptfenster extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
-
-		
+		contentPane.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				for(java.util.Iterator<JComponent> i = components.iterator(); i.hasNext();)
+				{
+					JComponent component = i.next();
+					component.setVisible(!component.isVisible());	
+				}
+				
+			}
+		});
+		components = new HashSet<JComponent>();
+	
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
@@ -57,57 +70,51 @@ public class Hauptfenster extends JFrame {
 		JButton btnSettings = new JButton("Einstellungen");
 		btnSettings.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
 			}
 		});
 		btnSettings.setBounds(10, 227, 116, 23);
 		components.add(btnSettings);
-		contentPane.add(btnSettings);
 		
-		JButton btnWerbungberbrcken = new JButton("Werbung \u00FCberbr\u00FCcken");
+		JButton btnWerbungberbrcken = new JButton("Werbung überbrücken");
 		btnWerbungberbrcken.setBounds(134, 193, 89, 23);
 		components.add(btnWerbungberbrcken);
-		contentPane.add(btnWerbungberbrcken);
+		
 		
 		JButton btnPause = new JButton("Pause");
 		btnPause.setBounds(136, 227, 89, 23);
-		contentPane.add(btnPause);
+		components.add(btnPause);
 		
 		JButton btnVorherigeSender = new JButton("vorherige sender");
 		btnVorherigeSender.setBounds(235, 227, 89, 23);
-		contentPane.add(btnVorherigeSender);
+		components.add(btnVorherigeSender);
 		
 		JButton btnSenderliste = new JButton("Senderliste");
 		btnSenderliste.setBounds(334, 227, 89, 23);
-		contentPane.add(btnSenderliste);
+		components.add(btnSenderliste);
 		
 		final JButton btnNchsteSender = new JButton("n\u00E4chste Sender");
 		btnNchsteSender.setBounds(334, 193, 89, 23);
 		btnNchsteSender.setVisible(false);
-		contentPane.add(btnNchsteSender);
+		components.add(btnNchsteSender);
 		
 		
 		JButton btnBalkenEntfernen = new JButton("Balken Entfernen");
 		btnBalkenEntfernen.setBounds(334, 155, 89, 23);
-		contentPane.add(btnBalkenEntfernen);
+		components.add(btnBalkenEntfernen);
 		
 		JButton btnStumm = new JButton("stumm");
 		btnStumm.setBounds(334, 125, 89, 23);
-		contentPane.add(btnStumm);
+		components.add(btnStumm);
 		
 		JSlider lautstärke = new JSlider();
 		lautstärke.setBounds(235, 88, 200, 26);
-		contentPane.add(lautstärke);
+		components.add(lautstärke);
 		
-	
+		initializeButtons();
+            
 		
-		contentPane.addMouseMotionListener(new MouseMotionAdapter() {
-			@Override
-			public void mouseMoved(MouseEvent e) {
-				btnNchsteSender.setVisible(true);
-				btnBalkenEntfernen.setVisible(true);
-				
-			}
-		});
+		
 	}
 	
     void initializeButtons()
